560399ee08562b327ce3356e50e86e81034636b4
Gennadiy99 <login841@gmail.com>
2025-04-04 22:12:59 +0200

    ended

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,8 +3,24 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
-  // write code here
+function makeInfinityAdder(param = 0) {
+  let sum = param;
+
+  function f(num) {
+    if (num === undefined) {
+      const sumInner = sum;
+
+      sum = 0;
+
+      return sumInner;
+    } else {
+      sum += num;
+
+      return f;
+    }
+  }
+
+  return f;
 }
 
 module.exports = makeInfinityAdder;
